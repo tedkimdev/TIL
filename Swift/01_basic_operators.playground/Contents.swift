64b10fa32f1103d4b780d1c09ead3aa67bdad7c5
aney <yenafirst91@gmail.com>
2017-03-05 23:24:49 +0900

    Add [Swift]basic operators

---
+++ Swift/01_basic_operators.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//str
+
+// variable 변수
+var number1 = 1
+number1 = 3
+
+// 상수
+let number2 = 2
+//number2 = 4 //error
+
+//str = 5 //error
+
+1 + 2
+5 - 3
+2 * 3
+10.0 / 2.5
+
+5 / 2
+5 % 2
+
+"abc" + "def"
