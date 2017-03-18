1bf4d9aef7fcbf920064eb05406cc603880d0f77
aney <yenafirst91@gmail.com>
2017-03-18 22:44:57 +0900

    [iOS] 다른 페이지로 이동

---
+++ segue_storyboard/segue_storyboard/CyanViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CyanViewController.swift
+//  segue_storyboard
+//
+//  Created by aney on 2017. 3. 18..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import UIKit
+
+class CyanViewController: UIViewController {
+  
+  @IBAction func buttonDidTap(_ sender: Any) {
+    self.dismiss(animated: true, completion: nil)
+  }
+
+}
---
+++ segue_storyboard/segue_storyboard/SilverViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SilverViewController.swift
+//  segue_storyboard
+//
+//  Created by aney on 2017. 3. 18..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import UIKit
+
+class SilverViewController: UIViewController {
+
+  @IBAction func buttonDidTap(_ sender: Any) {
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+}
